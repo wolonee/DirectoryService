e2e3cbc09f30165d5554af08845e1631c0f37b11
Tigl <a.tiglev@innopolis.university>
2026-04-01 14:10:49 +0300

    Вопрос описал в комментариях

--- DirectoryService/src/DirectoryService.Domain/Departments/ValueObjects/DepartmentName.cs
+++ DirectoryService/src/DirectoryService.Domain/Departments/ValueObjects/DepartmentName.cs
@@ -7,7 +7,9 @@ public record DepartmentName
     public const int MAX_LENGTH = 150;
     public const int MIN_LENGTH = 3;
     
-    public DepartmentName(string value)
+    public DepartmentName(string value) // конструкто у VO должен быть private?
+                                        // я помню что ты делал его приватным, но если он private, я не смогу юзать его при HasConversion.
+                                        // короче говоря, я правильно сделал?
     {
         Value = value;
     } 
