a944968905ee38e69c5f4bc04e12dbb2609f78a2
Tigl <a.tiglev@innopolis.university>
2026-05-01 17:19:15 +0300

    feat: add изолированную бд для тестов

--- DirectoryService/tests/DirectoryService.IntegrationTests/DirectoryTestWebFactory.cs
+++ DirectoryService/tests/DirectoryService.IntegrationTests/DirectoryTestWebFactory.cs
@@ -1,9 +1,20 @@
 ﻿using DirectoryService.Presentation;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Testcontainers.PostgreSql;
 
 namespace DirectoryService.IntegrationTests;
 
 public class DirectoryTestWebFactory : WebApplicationFactory<Program>
 {
-    
+    private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
+        .WithImage("postgres")
+        .WithDatabase("directory_service_db")
+        .WithUsername("postgres")
+        .WithPassword("postgres")
+        .Build();
+
+    public DirectoryTestWebFactory()
+    {
+        _dbContainer.StartAsync(); 
+    }
 }
