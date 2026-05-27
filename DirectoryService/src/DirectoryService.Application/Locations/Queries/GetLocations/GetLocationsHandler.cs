584b1301e05811168598adc4540ac7727f33d51b
Tigl <a.tiglev@innopolis.university>
2026-05-27 19:17:39 +0300

    feature: add Logger

--- DirectoryService/src/DirectoryService.Application/Locations/Queries/GetLocations/GetLocationsHandler.cs
+++ DirectoryService/src/DirectoryService.Application/Locations/Queries/GetLocations/GetLocationsHandler.cs
@@ -6,6 +6,7 @@ using DirectoryService.Infrastructure;
 using DirectoryService.Shared;
 using FluentValidation;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace DirectoryService.Application.Locations.Queries.GetLocations;
 
@@ -13,13 +14,16 @@ public class GetLocationsHandler : IQueryHandler<GetLocationsResponse, GetLocati
 {
     private readonly IReadDbContext _context;
     private readonly IValidator<GetLocationsQuery> _validator;
+    private readonly ILogger<GetLocationsHandler> _logger;
 
     public GetLocationsHandler(
         IReadDbContext context,
-        IValidator<GetLocationsQuery> validator)
+        IValidator<GetLocationsQuery> validator,
+        ILogger<GetLocationsHandler> logger)
     {
         _context = context;
         _validator = validator;
+        _logger = logger;
     }
 
     public async Task<Result<GetLocationsResponse, Errors>> Handle(
@@ -29,6 +33,7 @@ public class GetLocationsHandler : IQueryHandler<GetLocationsResponse, GetLocati
         var validationResult = await _validator.ValidateAsync(query, cancellationToken);
         if (!validationResult.IsValid)
         {
+            _logger.LogError("Validation Get Locations Failed: {Error}", validationResult.ToValidationErrors());
             validationResult.ToValidationErrors();
         }
         
