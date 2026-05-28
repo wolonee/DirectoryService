d4a0012b0242b981a8e461fbc1e28ee2373431b3
Tigl <a.tiglev@innopolis.university>
2026-05-28 21:12:21 +0300

    чутка передалал

---
+++ DirectoryService/src/DirectoryService.Contracts/Departments/Responses/GetDepartmentDto.cs
@@ -0,0 +1,13 @@
+﻿namespace DirectoryService.Contracts.Departments.Responses;
+
+public record GetDepartmentDto(
+    Guid Id,
+    string Name,
+    Guid? ParentId,
+    string Path,
+    int Depth,
+    int ChildrenCount,
+    bool IsActive,
+    DateTime CreatedAt,
+    DateTime UpdatedAt
+);
--- DirectoryService/src/DirectoryService.Contracts/Departments/Responses/GetTopDepartmentsByPositionsResponse.cs
+++ DirectoryService/src/DirectoryService.Contracts/Departments/Responses/GetTopDepartmentsByPositionsResponse.cs
@@ -1,19 +1,10 @@
 ﻿namespace DirectoryService.Contracts.Departments.Responses;
 
 public record GetTopDepartmentsByPositionsResponse(
-    Guid Id,
-    string Name,
-    Guid? ParentId,
-    string Path,
-    int Depth,
-    int ChildrenCount,
-    bool IsActive,
-    DateTime CreatedAt,
-    DateTime UpdatedAt
+    List<GetDepartmentDto> Departments
     // list Positions
     // int CountPositions
 );
-
 
 
 // public IReadOnlyList<DepartmentPosition> DepartmentPositions => _departmentPositions;
