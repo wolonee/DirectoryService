dfa62b6d98ec225ad9ac8b19fba7ae7482338b12
Tigl <a.tiglev@innopolis.university>
2026-05-29 17:58:58 +0300

    feature: add top departments with positions, я помню ты говорил что не надо оставлять код в комментах, но я это просто для запоминания оставлю

--- DirectoryService/src/DirectoryService.Application/Departments/Queries/GetByTopPositionsHandler.cs
+++ DirectoryService/src/DirectoryService.Application/Departments/Queries/GetByTopPositionsHandler.cs
@@ -4,6 +4,7 @@ using DirectoryService.Application.Abstractions;
 using DirectoryService.Application.Database;
 using DirectoryService.Contracts.Departments.Responses;
 using DirectoryService.Shared.Errors;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace DirectoryService.Application.Departments.Queries;
@@ -11,44 +12,89 @@ namespace DirectoryService.Application.Departments.Queries;
 public class GetByTopPositionsHandler : IQueryHandler<GetTopDepartmentsByPositionsResponse>
 {
     private readonly IDbConnectionFactory _dbConnectionFactory;
+    private readonly IReadDbContext _dbContext;
     private readonly ILogger<GetByTopPositionsHandler> _logger;
 
     public GetByTopPositionsHandler(
         IDbConnectionFactory dbConnectionFactory,
+        IReadDbContext dbContext,
         ILogger<GetByTopPositionsHandler> logger)
     {
         _dbConnectionFactory = dbConnectionFactory;
+        _dbContext = dbContext;
         _logger = logger;
     }
 
     public async Task<Result<GetTopDepartmentsByPositionsResponse, Errors>> Handle(
         CancellationToken cancellationToken = default)
     {
-        var connection = await _dbConnectionFactory.CreateConnectionAsync(cancellationToken);
+        // var connection = await _dbConnectionFactory.CreateConnectionAsync(cancellationToken);
 
         //language=sql
-        var result = await connection.QueryAsync<GetTopDepartmentsDepartmentDto>(
-            """
-            WITH position_counts AS ( SELECT department_id, COUNT(*) as count_positions
-                FROM department_positions
-                GROUP BY department_id
-            )
-            
-            SELECT d.id,
-                   d.name, 
-                   d.path,
-                   d.depth,
-                   d.is_active,
-                   d.created_at,
-                   d.updated_at,
-                   pc.count_positions
-                
-            FROM department as d
-            JOIN position_counts as pc ON pc.department_id = d.id
-            ORDER BY count_positions DESC 
-            LIMIT 5 OFFSET 0
-            """);
-        
-        return new GetTopDepartmentsByPositionsResponse(result.ToList());
+        // var result = await connection.QueryAsync<GetTopDepartmentsDepartmentDto>(
+        //     """
+        //     WITH position_counts AS ( SELECT department_id, COUNT(*) as count_positions
+        //         FROM department_positions
+        //         GROUP BY department_id
+        //     )
+        //     
+        //     SELECT d.id,
+        //            d.name, 
+        //            d.path,
+        //            d.depth,
+        //            d.is_active,
+        //            d.created_at,
+        //            d.updated_at,
+        //            pc.count_positions
+        //         
+        //     FROM department as d
+        //     JOIN position_counts as pc ON pc.department_id = d.id
+        //     ORDER BY count_positions DESC 
+        //     LIMIT 5 OFFSET 0
+        //     """);
+        //
+        // return new GetTopDepartmentsByPositionsResponse(result.ToList());
+
+        var deparmentsQuery = _dbContext.DepartmentsRead;
+        var deparmentPositionsQuery = _dbContext.DepartmentPositionsRead;
+        var positionsQuery = _dbContext.PositionsRead;
+
+        var result = await deparmentsQuery
+            .Select(d => new
+            {
+                Department = d,
+                CountPositions = deparmentPositionsQuery.Count(dp => dp.DepartmentId == d.Id),
+                Positions = deparmentPositionsQuery
+                    .Where(dp => dp.DepartmentId == d.Id)
+                    .Join(
+                        positionsQuery,
+                        dp => dp.PositionId,
+                        p => p.Id,
+                        (dp, p) => new GetTopDepartmentsDepartmentPositionDto
+                        {
+                            Id = p.Id,
+                            Speciality = p.Name.Speciality,
+                            Direction = p.Name.Direction,
+                            IsActive = p.IsActive,
+                        })
+                    .ToList(),
+            })
+            .OrderByDescending(x => x.CountPositions)
+            .Take(5)
+            .Select(x => new GetTopDepartmentsDepartmentDto()
+            {
+                Id = x.Department.Id,
+                Name = x.Department.DepartmentName.Value,
+                Path = x.Department.DepartmentPath.Value,
+                Depth = x.Department.Depth,
+                IsActive = x.Department.IsActive,
+                CreatedAt = x.Department.CreatedAt,
+                UpdatedAt = x.Department.UpdatedAt,
+                CountPositions = x.CountPositions,
+                Positions = x.Positions,
+            })
+            .ToListAsync(cancellationToken: cancellationToken);
+
+        return new GetTopDepartmentsByPositionsResponse(result);
     }
 }
--- DirectoryService/src/DirectoryService.Contracts/Departments/Responses/GetDepartmentDto.cs
+++ DirectoryService/src/DirectoryService.Contracts/Departments/Responses/GetDepartmentDto.cs
@@ -10,4 +10,14 @@ public record GetTopDepartmentsDepartmentDto
     public DateTime CreatedAt { get; init; }
     public DateTime UpdatedAt { get; init; }
     public int CountPositions { get; init; }
+    public List<GetTopDepartmentsDepartmentPositionDto> Positions { get; init; } = [];
+}
+
+public record GetTopDepartmentsDepartmentPositionDto
+{
+    public Guid Id { get; init; }
+    public string Speciality { get; init; } = string.Empty;
+    public string Direction { get; init; } = string.Empty;
+    public string Name { get; init; } = string.Empty;
+    public bool IsActive { get; init; }
 }
