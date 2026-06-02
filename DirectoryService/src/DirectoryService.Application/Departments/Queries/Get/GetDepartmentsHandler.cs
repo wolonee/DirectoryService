3ebd7e83eb2a9d1f74af5d356229a344df9e3cad
Tigl <a.tiglev@innopolis.university>
2026-06-02 17:42:06 +0300

    feature: add logger

--- DirectoryService/src/DirectoryService.Application/Departments/Queries/Get/GetDepartmentsHandler.cs
+++ DirectoryService/src/DirectoryService.Application/Departments/Queries/Get/GetDepartmentsHandler.cs
@@ -3,11 +3,20 @@ using DirectoryService.Application.Abstractions;
 using DirectoryService.Contracts.Departments.Responses;
 using DirectoryService.Domain.Departments;
 using DirectoryService.Shared.Errors;
+using Microsoft.Extensions.Logging;
 
 namespace DirectoryService.Application.Departments.Queries.Get;
 
 public class GetDepartmentsHandler : IQueryHandler<GetDepartmentsResponse, GetDepartmentsQuery>
 {
+    private readonly ILogger<GetDepartmentsHandler> _logger;
+
+    public GetDepartmentsHandler(
+        ILogger<GetDepartmentsHandler> logger)
+    {
+        _logger = logger;
+    }
+
     public Task<Result<GetDepartmentsResponse, Errors>> Handle(
         GetDepartmentsQuery query,
         CancellationToken cancellationToken = default)
