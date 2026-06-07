9f0dd94e0f9877532f7e5c8afa8a71965be72b08
Tigl <a.tiglev@innopolis.university>
2026-06-07 11:46:31 +0300

    feature: add validator

--- DirectoryService/src/DirectoryService.Application/Departments/Queries/GetDepartmentParentsByName/GetDepartmentParentsByNameHandler.cs
+++ DirectoryService/src/DirectoryService.Application/Departments/Queries/GetDepartmentParentsByName/GetDepartmentParentsByNameHandler.cs
@@ -3,27 +3,31 @@ using DirectoryService.Application.Abstractions;
 using DirectoryService.Application.Database;
 using DirectoryService.Contracts.Departments.Responses;
 using DirectoryService.Shared.Errors;
+using FluentValidation;
 using Microsoft.Extensions.Logging;
 
 namespace DirectoryService.Application.Departments.Queries.GetDepartmentParentsByName;
 
 public class GetDepartmentParentsByNameHandler : IQueryHandler<GetDepartmentParentsByNameResponse, GetDepartmentParentsByNameQuery>
 {
+    private readonly IValidator<GetDepartmentParentsByNameQuery> _validator;
     private readonly IDepartmentsRepository _departmentsRepository;
     private readonly ILogger<GetDepartmentParentsByNameHandler> _logger;
 
     public GetDepartmentParentsByNameHandler(
+        IValidator<GetDepartmentParentsByNameQuery> validator,
         IDepartmentsRepository departmentsRepository,
         ILogger<GetDepartmentParentsByNameHandler> logger)
     {
+        _validator = validator;
         _departmentsRepository = departmentsRepository;
         _logger = logger;
     }
 
-    public Task<Result<GetDepartmentParentsByNameResponse, Errors>> Handle(
+    public async Task<Result<GetDepartmentParentsByNameResponse, Errors>> Handle(
         GetDepartmentParentsByNameQuery query,
         CancellationToken cancellationToken = default)
     {
-        
+        var validationResult = await _validator.ValidateAsync(query, cancellationToken);
     }
 }
---
+++ DirectoryService/src/DirectoryService.Application/Departments/Queries/GetDepartmentParentsByName/GetDepartmentParentsByNameValidator.cs
@@ -0,0 +1,18 @@
+﻿using DirectoryService.Application.Validation;
+using DirectoryService.Shared.EntitiesErrors;
+using FluentValidation;
+
+namespace DirectoryService.Application.Departments.Queries.GetDepartmentParentsByName;
+
+public class GetDepartmentParentsByNameValidator : AbstractValidator<GetDepartmentParentsByNameQuery>
+{
+    public GetDepartmentParentsByNameValidator()
+    {
+        RuleFor(x => x.Request)
+            .NotNull()
+            .WithError(GeneralErrors.ValueIsRequired());
+        RuleFor(x => x.Request.Name)
+            .MinimumLength(2)
+            .WithError(GeneralErrors.MinimumLength(2, "name"));
+    }
+}
